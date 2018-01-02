695c62a191f59d4f29f4b2d203cf237abed19e96
MohammadYasir <m.yasir0001@gmail.com>
2018-01-02 20:27:14 +0530

    Sample App

---
+++ src/main/java/com/igt/test/todo/Task.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.igt.test.todo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ *
+ * @author myasir
+ */
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    private String title;
+    
+    @NotBlank
+    private String description;
+    
+    @NotBlank
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+}
---
+++ src/main/java/com/igt/test/todo/TodoController.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.igt.test.todo;
+
+import java.util.List;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author myasir
+ */
+@RestController
+@RequestMapping("/api")
+public class TodoController {
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    @GetMapping("/tasks")
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @GetMapping("/tasks/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable(value = "id") Long taskId) {
+        Task task = taskRepository.findOne(taskId);
+        if(task == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(task);
+    }
+
+    @PostMapping("/tasks")
+    public Task createTask(@Valid @RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @PutMapping("/tasks/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable(value = "id") Long noteId,
+                                           @Valid @RequestBody Task taskDetails) {
+        Task task = taskRepository.findOne(noteId);
+        if(task == null) {
+            return ResponseEntity.notFound().build();
+        }
+        task.setTitle(taskDetails.getTitle());
+        task.setDescription(taskDetails.getDescription());
+        task.setStatus(taskDetails.getStatus());
+
+        Task updatedNote = taskRepository.save(task);
+        return ResponseEntity.ok(updatedNote);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public ResponseEntity<Task> deleteTask(@PathVariable(value = "id") Long noteId) {
+        Task task = taskRepository.findOne(noteId);
+        if(task == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        taskRepository.delete(task);
+        return ResponseEntity.ok().build();
+    }
+}
